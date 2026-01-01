e4fbc11e4b7d1b5ebc6cf4b03c42467bce9eca19
Greg Brewer <greg@lessi.ai>
2026-01-01 12:56:22 -0500

    Upgrade project to .NET 10.0 and update package references; refactor code for improved clarity and consistency

--- samples/Sable.Samples.MultipleDatabases/Program.cs
+++ samples/Sable.Samples.MultipleDatabases/Program.cs
@@ -1,27 +1,27 @@
 ﻿// Copyright 2024 Bloomberg Finance L.P.
 // Distributed under the terms of the MIT license.
 
+using JasperFx;
+using Marten;
 using Sable.Extensions;
 using Sable.Samples.Core;
-using Marten;
-using Oakton;
 using Weasel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Host.ApplyOaktonExtensions();
+builder.Host.ApplyJasperFxExtensions();
 
 builder.Services.AddMartenWithSableSupport(_ =>
 {
-    var options = new StoreOptions
-    {
-        DatabaseSchemaName = "books"
-    };
+    var options = new StoreOptions { DatabaseSchemaName = "books" };
     options.Connection(builder.Configuration["Databases:Books:BasicTier"]);
     options.MultiTenantedDatabases(x =>
     {
         x.AddMultipleTenantDatabase(builder.Configuration["Databases:Books:GoldTier"], "books_gold")
             .ForTenants("gold1", "gold2");
-        x.AddSingleTenantDatabase(builder.Configuration["Databases:Books:SilverTier"], "books_silver");
+        x.AddSingleTenantDatabase(
+            builder.Configuration["Databases:Books:SilverTier"],
+            "books_silver"
+        );
     });
     options.AutoCreateSchemaObjects = AutoCreate.None;
     options.Schema.For<Book>();
@@ -30,16 +30,19 @@ builder.Services.AddMartenWithSableSupport(_ =>
 
 builder.Services.AddMartenStoreWithSableSupport<IOtherDocumentStore>(_ =>
 {
-    var options = new StoreOptions
-    {
-        DatabaseSchemaName = "orders"
-    };
+    var options = new StoreOptions { DatabaseSchemaName = "orders" };
     options.Connection(builder.Configuration["Databases:Orders:BasicTier"]);
     options.MultiTenantedDatabases(x =>
     {
-        x.AddMultipleTenantDatabase(builder.Configuration["Databases:Books:GoldTier"], "orders_gold")
+        x.AddMultipleTenantDatabase(
+                builder.Configuration["Databases:Books:GoldTier"],
+                "orders_gold"
+            )
             .ForTenants("gold1", "gold2");
-        x.AddSingleTenantDatabase(builder.Configuration["Databases:Books:SilverTier"], "orders_silver");
+        x.AddSingleTenantDatabase(
+            builder.Configuration["Databases:Books:SilverTier"],
+            "orders_silver"
+        );
     });
     options.AutoCreateSchemaObjects = AutoCreate.None;
     options.Schema.For<Order>();
@@ -49,4 +52,4 @@ builder.Services.AddMartenStoreWithSableSupport<IOtherDocumentStore>(_ =>
 var app = builder.Build();
 app.MapGet("/", () => "💪🏾");
 
-return await app.RunOaktonCommands(args);
+return await app.RunJasperFxCommands(args);
